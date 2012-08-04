40783ec7337e11c4a3afa5f7123c484f774ff138
Daniel Cassidy <mail@danielcassidy.me.uk>
2012-08-04 02:54:14 +0100

    It seems to sometimes be necessary to wait at least one frame before playing audio, for some reason.

--- src/nme_gapless_mp3_example/Mono.hx
+++ src/nme_gapless_mp3_example/Mono.hx
@@ -1,10 +1,17 @@
 package nme_gapless_mp3_example;
 
+import flash.events.Event;
 import nme.installer.Assets;
 
 class Mono {
     public static function main() {
+        flash.Lib.current.stage.addEventListener(Event.ENTER_FRAME, onEnterFrame);
+    }
+
+    static function onEnterFrame(event:Event) {
         var music = Assets.getSound("assets/Mono.mp3");
         music.play(0, 2147483647);
+
+        flash.Lib.current.stage.removeEventListener(Event.ENTER_FRAME, onEnterFrame);
     }
 }
